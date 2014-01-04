6223710ff599cebe0ddc7971670588e08320fe31
Icelle <icelleku@gmail.com>
2014-01-03 19:01:45 -0500

    made route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 CarListing::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :cars
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
